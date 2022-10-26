600a1e98c3593d27a47b1c9901d9806509c65929
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-26 23:50:56 +0100

    Add files via upload

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -61,7 +61,7 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
     score_old = (double*) malloc(sizeof(double) * numNodes);
     bool converged = false;
     double aux = 0;
-    int chunk_size = (numNodes + 8000 - 1) / 8000;
+    int chunk_size = (numNodes + 80000 - 1) / 80000;
     // double densidade = 2 * num_edges(g) / numNodes;
     while (!converged) {
       global_diff = 0;
