6421146f2babe114a41cf5eefc6d7919a1a7ca20
zezezedididi <jose.eduardo.diniz@tecnico.ulisboa.pt>
2022-10-27 17:35:44 +0100

    fabs +global diff inside diff

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -101,18 +101,13 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
         for (int i = 0; i < numNodes; ++i) {
           solution[i] += aux;
           double aux1 = solution[i], aux2 = score_old[i];
-
-          if (aux1 > aux2) {
-            mydiff += aux1 - aux2;
-          }
-          else {
-            mydiff += aux2 - aux1;
-          }
+          mydiff = fabs(aux1-aux2);
           
+          #pragma omp atomic
+          global_diff += mydiff;
         }
       
-        #pragma omp atomic
-        global_diff += mydiff;
+       
       }
 
       converged = (global_diff < convergence);
