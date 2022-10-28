2acb88087d58c15513757c9fdb6f0ec95e767472
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-28 02:18:27 +0100

    Add files via upload

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -257,4 +257,51 @@ void bfs_hybrid(Graph graph, solution* sol)
     //
     // You will need to implement the "hybrid" BFS here as
     // described in the handout.
+    vertex_set list1;
+    vertex_set list2;
+    vertex_set_init(&list1, graph->num_nodes);
+    vertex_set_init(&list2, graph->num_nodes);
+
+    vertex_set* frontier = &list1;
+    vertex_set* new_frontier = &list2;
+
+    // initialize all nodes to NOT_VISITED
+    # pragma omp parallel for schedule(dynamic, 100)
+    for (int i=0; i<graph->num_nodes; i++)
+        sol->distances[i] = NOT_VISITED_MARKER;
+
+    // setup frontier with the root node
+    frontier->vertices[frontier->count++] = ROOT_NODE_ID;
+    sol->distances[ROOT_NODE_ID] = 0;
+
+    while (frontier->count != 0) {
+
+#ifdef VERBOSE
+        double start_time = CycleTimer::currentSeconds();
+#endif
+
+        vertex_set_clear(new_frontier);
+        if (frontier->count > 100) {
+            bottom_up_step(graph, frontier, new_frontier, sol->distances);
+        }
+        else {
+            top_down_step(graph, frontier, new_frontier, sol->distances);
+        }
+
+#ifdef VERBOSE
+    double end_time = CycleTimer::currentSeconds();
+    printf("frontier=%-10d %.4f sec\n", frontier->count, end_time - start_time);
+#endif
+
+        // swap pointers
+        //vertex_set* tmp;
+        //__sync_bool_compare_and_swap ((vertex_set*) tmp, (vertex_set*) frontier, (vertex_set*) new_frontier);
+
+        vertex_set* tmp = frontier;
+        frontier = new_frontier;
+        new_frontier = tmp;
+    }
+
+    free(list1.vertices);
+    free(list2.vertices);
 }
