0d07fb208d0ee344226b602df879997e0daea048
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-10-30 18:56:40 +0000

    Add files via upload

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -32,7 +32,7 @@ void top_down_step(
     vertex_set* new_frontier,
     int* distances)
 {
-    # pragma omp parallel for schedule(dynamic, (frontier->count + 80 - 1) / 80)
+    # pragma omp parallel for schedule(dynamic, (frontier->count + 24 - 1) / 24)
     for (int i = 0; i < frontier->count; i++) {
         int node = frontier->vertices[i];
 
