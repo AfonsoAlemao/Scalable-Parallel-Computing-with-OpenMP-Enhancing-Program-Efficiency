bc52f5cea597a12866eff9b19efb817ef54d7113
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-11-01 12:57:57 +0000

    Add files via upload

--- pagerank/page_rank.cpp
+++ pagerank/page_rank.cpp
@@ -39,8 +39,8 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
      }
    */
     
-  // initialize vertex weights to uniform probability. Double
-  // precision scores are used to avoid underflow for large graphs
+  /* Initialize vertex weights to uniform probability. Double
+  precision scores are used to avoid underflow for large graphs. */
 
   int numNodes = num_nodes(g);
   double equal_prob = 1.0 / numNodes, partialsum =  (1.0-damping) / numNodes;
@@ -53,6 +53,14 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
   double aux = 0;
   int chunk_size = (numNodes + 8000 - 1) / 8000;
 
+
+  /* Launches “team” of OpenMP threads and partitions iterations among them. We choose dynamic schedule for
+  all parallel loops of our program because the workload is not balanced across iterations. When a thread
+  finished one chunk, it is dynamically assigned another. The chunk size was chosen in order to guarantee enough 
+  granularity to balance the workload beetween threads and also to avoid that the overhead associated with the communication 
+  between threads and the launching of these be harmful to the performance of the program.*/
+
+  /* Initializations. */
   #pragma omp parallel for schedule(dynamic, chunk_size)
   for (int i = 0; i < numNodes; ++i) {
     score_old[i] = equal_prob;
@@ -68,21 +76,22 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
   while (!converged) {
     global_diff = 0;
     aux = 0;
+    /* To avoid that teams of OpenMP threads can be created and disbanded (or put in wait state) many times, 
+    we want that a team created once are reused many times. Not active threads are put in wait state, 
+    potentially reducing disbanding cost.  */
     #pragma omp parallel shared(aux, numNodes, score_old, solution, global_diff)
     {
       double mydiff = 0;
       double myaux = 0;
 
-      // compute score_new[vi] for all nodes vi:
-      //score_new[vi] = sum over all nodes vj reachable from incoming edges { score_old[vj] / number of edges leaving vj  }
+      /* Removing implicit barrier */
       # pragma omp for schedule(dynamic, chunk_size) nowait
       for (int i = 0; i < numNodes; ++i) {
         double auxiliar = 0;
         const Vertex* start = incoming_begin(g, i);
         const Vertex* end = incoming_end(g, i);
         for (const Vertex* v = start; v != end; v++) {
-          // Edge (i, *v);
-          // auxiliar += score_old[*v] / outgoingsize[*v];
+          /* Edge (i, *v) */
           auxiliar += auxiliarvertex[*v];
         }
         solution[i] = (damping * auxiliar) + partialsum;
@@ -92,14 +101,19 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
       }
 
       if (myaux != 0) {
+        /* Must use atomic to avoid data races. */
         #pragma omp atomic
         aux += myaux;
       }
+      /* Need to synchronize because the next part requires a valid aux value. */
       #pragma omp barrier
 
+      /* Removing implicit barrier. */
       #pragma omp for schedule(dynamic, chunk_size) nowait
       for (int i = 0; i < numNodes; ++i) {
         solution[i] += aux;
+
+        /* mydiff += abs(aux1 - aux2) */
         double aux1 = solution[i], aux2 = score_old[i];
         if (aux1 > aux2) {
           mydiff += aux1 - aux2;
@@ -107,6 +121,8 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
         else {
           mydiff += aux2 - aux1;
         }
+
+        /* Initializations. */
         score_old[i] = aux1;
         if (outgoingsize[i] == 0) {
            auxiliarvertex[i] = damping_per_numNodes * score_old[i];
@@ -117,8 +133,11 @@ void pageRank(Graph g, double* solution, double damping, double convergence)
        
       }
 
+      /* Must use atomic to avoid data races. */
       #pragma omp atomic
       global_diff += mydiff;
+
+      /* Need to synchronize because the next part requires a valid global_diff value. */
       #pragma omp barrier
     }
 
