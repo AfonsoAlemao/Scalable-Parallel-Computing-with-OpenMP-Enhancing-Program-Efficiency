cea445b03eb2afe0d1a47047dd3d70ade86e746f
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-11-01 16:51:45 +0000

    Add files via upload

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -92,7 +92,7 @@ void top_down_step(
     }
 
     
-    # pragma omp parallel for schedule(dynamic, (frontier->count + 800 - 1) / 800)
+    # pragma omp parallel for schedule(dynamic, (numNodes + 800 - 1) / 800)
     for (int k = 0; k < numNodes; k++) {
         if (distances[k] == dist_frontier + 1) {
             int index = 0;
