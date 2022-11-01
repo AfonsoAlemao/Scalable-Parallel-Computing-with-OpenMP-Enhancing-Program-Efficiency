15603b6438312691b6d004d0bde1a441fe29bf40
Rpcd <67294809+RuiDaniel@users.noreply.github.com>
2022-11-01 21:23:13 +0000

    Add files via upload

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -30,25 +30,24 @@ void top_down_step(
     Graph g,
     vertex_set* frontier,
     vertex_set* new_frontier,
-    int* distances)
+    int* distances, int* outgoing_size, int* outgoing_starts)
 {
     int dist_frontier = distances[frontier->vertices[0]];
     int numNodes = g->num_nodes;
-    
+    int frontier_count = frontier->count;
 
-
-    //printf("Frontier count %d\n", frontier->count);
-    if (frontier->count > 1000) {
-        # pragma omp parallel for schedule(dynamic, (frontier->count + 8000 - 1) / 8000)
-        for (int i = 0; i < frontier->count; i++) {
-            int myfrontiers[outgoing_size(g, frontier->vertices[i])];
+    //printf("Frontier count %d\n", frontier_count);
+    if (frontier_count > 1000) {
+        # pragma omp parallel for schedule(dynamic, (frontier_count + 8000 - 1) / 8000)
+        for (int i = 0; i < frontier_count; i++) {
+            int myfrontiers[outgoing_size[frontier->vertices[i]]];
             int mycount = 0;
             int node = frontier->vertices[i];
             // printf("Tou no vertice %d\n", node);
-            int start_edge = g->outgoing_starts[node];
+            int start_edge = outgoing_starts[node];
             int end_edge = (node == numNodes - 1)
                             ? g->num_edges
-                            : g->outgoing_starts[node + 1];
+                            : outgoing_starts[node + 1];
                             
             // attempt to add all neighbors to the new frontier
             // printf("Os meus vizinhos:\n");
@@ -75,15 +74,15 @@ void top_down_step(
 
     }
     else {
-        for (int i = 0; i < frontier->count; i++) {
-            int myfrontiers[outgoing_size(g, frontier->vertices[i])];
+        for (int i = 0; i < frontier_count; i++) {
+            int myfrontiers[outgoing_size[frontier->vertices[i]]];
             int mycount = 0;
             int node = frontier->vertices[i];
             // printf("Tou no vertice %d\n", node);
-            int start_edge = g->outgoing_starts[node];
+            int start_edge = outgoing_starts[node];
             int end_edge = (node == numNodes - 1)
                             ? g->num_edges
-                            : g->outgoing_starts[node + 1];
+                            : outgoing_starts[node + 1];
                             
             // attempt to add all neighbors to the new frontier
             // printf("Os meus vizinhos:\n");
@@ -142,18 +141,26 @@ void top_down_step(
 // distance to the root is stored in sol.distances.
 void bfs_top_down(Graph graph, solution* sol) {
 
+    int numNodes = graph->num_nodes;
     vertex_set list1;
     vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    vertex_set_init(&list1, numNodes);
+    vertex_set_init(&list2, numNodes);
 
     vertex_set* frontier = &list1;
     vertex_set* new_frontier = &list2;
 
+    int *outgoingsize, *outgoingstarts;
+    outgoingsize = (int*) malloc(sizeof(int) * numNodes);
+    outgoingstarts = (int*) malloc(sizeof(int) * numNodes);
+
     // initialize all nodes to NOT_VISITED
     # pragma omp parallel for
-    for (int i=0; i<graph->num_nodes; i++)
+    for (int i = 0; i < numNodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
+        outgoingsize[i] = outgoing_size(graph, i);
+        outgoingstarts[i] = graph->outgoing_starts[i];
+    }
 
     // setup frontier with the root node
     frontier->vertices[frontier->count++] = ROOT_NODE_ID;
@@ -167,7 +174,7 @@ void bfs_top_down(Graph graph, solution* sol) {
 
         vertex_set_clear(new_frontier);
 
-        top_down_step(graph, frontier, new_frontier, sol->distances);
+        top_down_step(graph, frontier, new_frontier, sol->distances, outgoingsize, outgoingstarts);
 
 #ifdef VERBOSE
     double end_time = CycleTimer::currentSeconds();
@@ -390,17 +397,19 @@ void bfs_bottom_up(Graph graph, solution* sol)
     // As was done in the top-down case, you may wish to organize your
     // code by creating subroutine bottom_up_step() that is called in
     // each step of the BFS process.
+    int numNodes = graph->num_nodes;
+
     vertex_set list1;
     vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    vertex_set_init(&list1, numNodes);
+    vertex_set_init(&list2, numNodes);
 
     vertex_set* frontier = &list1;
     vertex_set* new_frontier = &list2;
 
     // initialize all nodes to NOT_VISITED
     # pragma omp parallel for
-    for (int i=0; i<graph->num_nodes; i++)
+    for (int i = 0; i < numNodes; i++)
         sol->distances[i] = NOT_VISITED_MARKER;
 
     // setup frontier with the root node
@@ -439,18 +448,27 @@ void bfs_hybrid(Graph graph, solution* sol)
     //
     // You will need to implement the "hybrid" BFS here as
     // described in the handout.
+    int numNodes = graph->num_nodes;
+
     vertex_set list1;
     vertex_set list2;
-    vertex_set_init(&list1, graph->num_nodes);
-    vertex_set_init(&list2, graph->num_nodes);
+    vertex_set_init(&list1, numNodes);
+    vertex_set_init(&list2, numNodes);
 
     vertex_set* frontier = &list1;
     vertex_set* new_frontier = &list2;
+    
+    int *outgoingsize, *outgoingstarts;
+    outgoingsize = (int*) malloc(sizeof(int) * numNodes);
+    outgoingstarts = (int*) malloc(sizeof(int) * numNodes);
 
     // initialize all nodes to NOT_VISITED
     # pragma omp parallel for
-    for (int i=0; i<graph->num_nodes; i++)
+    for (int i = 0; i < numNodes; i++) {
         sol->distances[i] = NOT_VISITED_MARKER;
+        outgoingsize[i] = outgoing_size(graph, i);
+        outgoingstarts[i] = graph->outgoing_starts[i];
+    }
 
     // setup frontier with the root node
     frontier->vertices[frontier->count++] = ROOT_NODE_ID;
@@ -465,11 +483,11 @@ void bfs_hybrid(Graph graph, solution* sol)
         vertex_set_clear(new_frontier);
         // Top down complexity: number of frontier nodes
         // Bottom up complexity: number of nodes outside the bfs ~ number of nodes - number of frontier nodes
-        if (frontier->count * frontier->count > graph->num_nodes) {
+        if (frontier->count * frontier->count > numNodes) {
             bottom_up_step(graph, frontier, new_frontier, sol->distances);
         }
         else {
-            top_down_step(graph, frontier, new_frontier, sol->distances);
+            top_down_step(graph, frontier, new_frontier, sol->distances, outgoingsize, outgoingstarts);
         }
 
 #ifdef VERBOSE
